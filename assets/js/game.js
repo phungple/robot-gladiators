fdc45a82c7d2f9aca6c94a01ef1e249d02dfbf7a
phungle <phungple2015@gmail.com>
2022-03-06 19:45:03 -0600

    loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth, playerMoney);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -24,15 +21,15 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
 
         // check enemy's health 
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died! ");
+            window.alert(enemyNames + " has died! ");
         }
         else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left. ");
         }
 
         // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
@@ -40,7 +37,7 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remainning."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remainning."
         );
 
         // check player's health
@@ -63,11 +60,13 @@ var fight = function() {
         }
         // if no (false), ask question again by running fight() again
         else {
-            fight();
-        }   
+            for(var i = 0; i < enemyNames.length; i++) {
+                fight(enemyNames[i]);
+            }
+        }
     } else {
         window.alert("You need to choose a valid option. Try again! ");
     }
 };
-
 fight();
+
