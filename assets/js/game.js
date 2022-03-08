24c5896b4b132cc108842cfa53cc7c15a064305f
phungle <phungple2015@gmail.com>
2022-03-08 13:41:41 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -89,6 +89,17 @@ var startGame = function() {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+
+            // if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                // ask if player wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                // if yes, take them to the shop() function
+                if(storeConfirm) {
+                    shop();
+                }
+            }
         }
         // if player isn't alive, stop the game
         else {
@@ -120,6 +131,13 @@ var endGame = function() {
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
+};
+// feature/shop
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRAGE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
 };
 // start the game when the page loads
 startGame();
